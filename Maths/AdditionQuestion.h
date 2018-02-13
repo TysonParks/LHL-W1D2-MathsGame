f82e8f6122be557c03f75d04ffe728a5a58d3f41
tysonparks <typarks@gmail.com>
2018-02-13 14:17:51 -0800

    Create AdditionQuestion class

---
+++ Maths/AdditionQuestion.h
@@ -0,0 +1,13 @@
+//
+//  AdditionQuestion.h
+//  Maths
+//
+//  Created by Tyson Parks on 2/13/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdditionQuestion : NSObject
+
+@end
---
+++ Maths/AdditionQuestion.m
@@ -0,0 +1,13 @@
+//
+//  AdditionQuestion.m
+//  Maths
+//
+//  Created by Tyson Parks on 2/13/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import "AdditionQuestion.h"
+
+@implementation AdditionQuestion
+
+@end
