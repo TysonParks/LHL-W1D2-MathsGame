a6925f4e688ccce04029d6b32c92fc38bf94823a
tysonparks <typarks@gmail.com>
2018-02-13 17:15:43 -0800

    Create InputHandler class

---
+++ Maths/InputHandler.h
@@ -0,0 +1,13 @@
+//
+//  InputHandler.h
+//  Maths
+//
+//  Created by Tyson Parks on 2/13/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InputHandler : NSObject
+
+@end
---
+++ Maths/InputHandler.m
@@ -0,0 +1,13 @@
+//
+//  InputHandler.m
+//  Maths
+//
+//  Created by Tyson Parks on 2/13/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import "InputHandler.h"
+
+@implementation InputHandler
+
+@end
