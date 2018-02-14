cd49a9b153814cdd5f237786a85395894909cc91
tysonparks <typarks@gmail.com>
2018-02-13 17:37:12 -0800

    Commented code

--- Maths/main.m
+++ Maths/main.m
@@ -24,26 +24,31 @@ int main(int argc, const char * argv[]) {
             // Log question from AdditionQuestion
             NSLog(@"%@\n", firstQuestion.question);
             
-            
+            // Initialize an InputHandler and use it to get user's input
             InputHandler *getUsersAnswer = [[InputHandler alloc]init];
             NSString *userAnswer = [getUsersAnswer returnUserInputAsString];
             
+            // Log user's output
             NSLog(@"You entered %@", userAnswer);
             
             //Check if user's answer is correct?
             BOOL correctAnswer;
-            
+            // End game if user types 'quit'
             if([userAnswer isEqualToString:@"quit"]) {
                 gameOn = NO;
                 continue;
+            // Print Right! for correct answer
             } else if ([userAnswer isEqualToString:firstQuestion.answer]) {
                 correctAnswer = YES;
                 NSLog(@"Right!");
+            //Print Wrong! for incorrect answer and give correct answer
             } else {
                 correctAnswer = NO;
                 NSLog(@"Wrong!");
+                NSLog(@"The correct answer is %@", firstQuestion.answer);
             }
-            NSLog(@"The correct answer is %@", firstQuestion.answer);
+            
+            
             
             
         }
