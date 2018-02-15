212c436afaa2605e041b878a9c9d84427ab3fbc7
tysonparks <typarks@gmail.com>
2018-02-14 19:18:51 -0800

    Created QuestionFactory class

---
+++ Maths/QuestionFactory.h
@@ -0,0 +1,13 @@
+//
+//  QuestionFactory.h
+//  Maths
+//
+//  Created by Tyson Parks on 2/14/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuestionFactory : NSObject
+
+@end
---
+++ Maths/QuestionFactory.m
@@ -0,0 +1,13 @@
+//
+//  QuestionFactory.m
+//  Maths
+//
+//  Created by Tyson Parks on 2/14/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import "QuestionFactory.h"
+
+@implementation QuestionFactory
+
+@end
